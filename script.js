afdebb72c301a51835b60b50822b503f526fd29c
Adrian Ramirez <Adrian.Ramirez99>
2021-02-27 03:08:09 -0500

    Finished adding and removing Rows & Columns
    Also finished selecting color from the drop down menu

--- script.js
+++ script.js
@@ -1,31 +1,87 @@
-let numRows = 0;
-let numCols = 0;
-let colorSelected;
+let num_Rows = 1;
+let num_Cols = 1;
+let colorSelected = "#ffffff";
+
+
+// Creates a Cell (Helper Function)
+function initial_Color(new_Cell) {
+    new_Cell.classList.add("no_Color");
+    new_Cell.style.backgroundColor = "#ffffff";
+    return new_Cell;
+}
 
 // Adds a row
 function addR() {
-    alert("Clicked Add Row")
+    let grid = document.getElementById("table");
+    let row = document.createElement("tr");
+
+    for (let x = 0; x < num_Cols; x++)
+    {
+        let new_Cell = document.createElement("td");
+        initial_Color(new_Cell);
+
+        row.appendChild(new_Cell);
+    }
+
+    grid.appendChild(row);
+    num_Rows++;
 }
 
 // Adds a column
 function addC() {
-    alert("Clicked Add Column")
-}
+    let rows = document.querySelectorAll("tr");
+
+    let row_Counter = 0;
+
+    for (let x = 0; x < num_Rows; x++) {
+        let new_Cell = document.createElement("td");
+        initial_Color(new_Cell);
+
+        rows[row_Counter].appendChild(new_Cell);
+        row_Counter++;
+    }
+
+    num_Cols++;
+}   
 
 // Removes a Row
 function removeR() {
-    alert("Clicked Remove Row")
+    if (num_Rows <= 1) {
+        alert("Cannot delete anymore Rows")
+    }
+
+    else {
+        let grid = document.getElementById("table");
+        grid.deleteRow(num_Rows - 1);
+   
+        num_Rows--;
+    }
 }
 
-// Remove a Column
+// Removes a Column
 function removeC() {
-    alert("Clicked Remove Col")
+    if (num_Cols <= 1) {
+        alert("Cannot Remove anymore Columns")
+    }
+
+    else {
+        let rows = document.querySelectorAll("tr");
+        let row_Counter = 0;
+    
+        for (let x = 0; x < num_Rows; x++) {
+            rows[row_Counter].removeChild(rows[row_Counter].lastChild);
+            row_Counter++;
+        }
+    
+        num_Cols--;
+    }
+
 }
 
 // Sets global variable for selected color
-function selected() {
-    colorSelected = document.getElementById("SelectedID").value;
-    console.log(colorSelected);
+function selectedColor(color) {
+    colorSelected = color;
+    console.log(color);
 }
 
 // Fills all uncolored squares with a specified color
@@ -35,11 +91,10 @@ function fillU() {
 
 // Sets all squares to the specified color
 function fill() {
-    alert("Clicked Fill All")
+    alert("Clicked Fill")
 }
 
 // Clears all squares
 function clearAll() {
     alert("Clicked Clear All")
 }
-
